23482b64cfff7babcdffd0abf27fde08c81606af
LEB31 <93269935+LEB31@users.noreply.github.com>
2022-04-18 09:46:31 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+# Tkinter Input/Output 4/18/2022
+import tkinter as Tk
+from tkinter import *
+
+win = Tk()
+# Create a window
+win.title = "Title"
+win.geometry('600x500')
+
+# Exit Button
+exit = Button(win, text="Exit", command=win.quit)
+exit.place(height=20, x=300, y=300)
+
+win.mainloop()
