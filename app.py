f7a83dbd2031a612dfcbec5b5ab5829e43f3bfef
LEB31 <93269935+LEB31@users.noreply.github.com>
2022-04-20 10:14:45 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # Tkinter Input/Output 4/18/2022
 # Create a window
 from tkinter import *
-from turtle import bgcolor
+
 
 win = Tk()
 
@@ -10,6 +10,7 @@ def level1():
    first.geometry('600x500')
    first.title('Start')
    first.configure(bg='light pink')
+   # Exit the Program
    btn2 = Button(first, text = 'Exit', command=quit)
    btn2.place(width= 100, height= 50, x=250, y=250)
 
@@ -17,9 +18,20 @@ def level1():
 win.geometry('600x500')
 win.title('Welcome!')
 win.configure(bg='light blue')
-
-btn = Button(win, text ='Start', command=level1)
-btn.place(width= 100, height= 50, x=250, y=250)
+# Label (Log in)
+lb = Label(win, text='Log In')
+lb.place(x=280, y=200)
+# Label (Username)
+lbun = Label(win, text='Username:')
+lbun.place(x=210,y=250)
+# Enter Username
+un = Entry(win)
+un.place(x=270,y=250)
+# Variables
+user = un.get()
+# Open Second Window
+btn = Button(win, text ='Submit', command=level1)
+btn.place(x=275, y=300)
 
 win.mainloop()
 
