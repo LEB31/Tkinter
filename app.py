9e986be701633686e868176d68b6303a00de7212
LEB31 <93269935+LEB31@users.noreply.github.com>
2022-04-21 09:58:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,61 @@
 # Tkinter Input/Output 4/18/2022
 # Create a window
 from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 
 win = Tk()
 
 def level1():
    first=Toplevel()
+   first.configure(bg="#81BCA9")
    first.geometry('600x500')
    first.title('Start')
-   first.configure(bg='light pink')
+   # List 
+   name = []
+   p = []
+   # Variables 
+   user = un.get()
+   name.append(user)
+   up = pw.get()
+   p.append(up)
+   # Welcome User
+   lb2 = Label(first, text = 'Hello ' + name[0] + '!')
+   lb2.place(x=265, y= 150)
+
    # Exit the Program
-   btn2 = Button(first, text = 'Exit', command=quit)
+   btn2 = Button(first, text = 'START', command=level2)
    btn2.place(width= 100, height= 50, x=250, y=250)
+
+def level2():
+   start=Toplevel()
+   start.configure(bg="#EDEFA2")
+   start.geometry('600x500')
+   start.title("Let's Go!")
 
 
 win.geometry('600x500')
 win.title('Welcome!')
-win.configure(bg='light blue')
+win.configure(bg="#BC8194")
 # Label (Log in)
 lb = Label(win, text='Log In')
 lb.place(x=280, y=200)
 # Label (Username)
 lbun = Label(win, text='Username:')
-lbun.place(x=210,y=250)
+lbun.place(x=205,y=250)
 # Enter Username
 un = Entry(win)
 un.place(x=270,y=250)
-# Variables
-user = un.get()
+# Label (Password)
+lbun1 = Label(win, text='Password:')
+lbun1.place(x=205,y=275)
+# Enter Username
+pw = Entry(win, show='*')
+pw.place(x=270,y=275)
 # Open Second Window
 btn = Button(win, text ='Submit', command=level1)
-btn.place(x=275, y=300)
+btn.place(x=275, y=315)
 
 win.mainloop()
 
